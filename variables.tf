e5e9c0c40bc2f6c96e3179b78b5b7c058c868f21
apps <23049337+yevgenis-shapiro-max2006@users.noreply.github.com>
2026-09-25 22:03:10 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,21 +1,26 @@
 
 variable "region" {
+  type    = string
   default = "us-west-2"
 }
 
 variable "ami_id" {
+  type    = string
   default = "ami-0fc5d935ebf8bc3bc" # Ubuntu 22.04
 }
 
 variable "instance_type" {
+  type    = string
   default = "t3.medium"
 }
 
 variable "master_count" {
+  type    = number
   default = 3
 }
 
 variable "worker_count" {
+  type    = number
   default = 2
 }
 
@@ -27,31 +32,36 @@ variable "root_volume_size" {
 
 variable "root_volume_type" {
   type        = string
-  description = "Type of root volume (e.g., gp3, gp2, io1)"
+  description = "Type of root volume"
   default     = "gp3"
 }
 
 variable "ssh_allowed_cidr" {
+  type    = string
   default = "0.0.0.0/0"
 }
 
-variable "https_allowed_cidr" {
+variable "web_allowed_cidr" {
+  type    = string
   default = "0.0.0.0/0"
 }
 
 variable "security_group_name" {
+  type    = string
   default = "k3s-cluster-sg"
 }
 
 variable "security_group_description" {
+  type    = string
   default = "K3s cluster security group"
 }
 
 variable "ssh_public_key_path" {
+  type    = string
   default = "/home/ubuntu/.ssh/id_rsa.pub"
 }
 
 variable "ssh_private_key_path" {
+  type    = string
   default = "/home/ubuntu/.ssh/id_rsa"
 }
-
