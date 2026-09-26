ec79cc3246f8050fd5bb11b7d693cdef289c7afd
apps <23049337+yevgenis-shapiro-max2006@users.noreply.github.com>
2026-09-26 15:19:47 +0300

    update
    Removed an empty line at the start of the file.

--- output.tf
+++ output.tf
@@ -1,5 +1,4 @@
 
-
 output "k3s_master_ips" {
   description = "Public IPs of all master nodes"
   value       = concat(aws_instance.k3s_master_primary[*].public_ip, aws_instance.k3s_master_additional[*].public_ip)
